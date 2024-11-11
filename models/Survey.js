3d48ce72397f87bacce60b93628c5be0065b306e
notjawad404 <jawadali000123@gmail.com>
2024-11-11 13:57:44 +0500

    Update survey model

--- models/Survey.js
+++ models/Survey.js
@@ -2,14 +2,12 @@ const mongoose = require('mongoose');
 
 const surveySchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  progress: {
     step1: { type: String },
     step2: {
       Comfort: { type: Number },
       Looks: { type: Number },
       Price: { type: Number }
-    }
-  },
+    },
   status: { type: String, default: 'in-progress' }
 }, { timestamps: true });
 
